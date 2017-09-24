71ee5a6d1d314305390da824e3489993dbf920af
cho5j <31690047+cho5j@users.noreply.github.com>
2017-09-24 19:40:39 +0900

    test

---
+++ 01_basic_operators.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//str
+var number1 = 1
+
+let number2 = 2
+
+number1 = number2
+
+
+
+
+
+1 + 2
+5 - 3
+10.0/2.5
+5 % 2
+
+"Hello" + " world"
+
+
