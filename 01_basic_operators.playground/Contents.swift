69473e66cf5388c034487de4920ecbf81a3c6a78
cho5j <31690047+cho5j@users.noreply.github.com>
2017-09-24 20:22:09 +0900

    test2

--- 01_basic_operators.playground/Contents.swift
+++ 01_basic_operators.playground/Contents.swift
@@ -21,4 +21,41 @@ number1 = number2
 
 "Hello" + " world"
 
+var number = 1
+
+var str2:String = "helllo"
+var number4:Int = 4
+10
+"10"
+10 + 11
+
+//문자를 숫자로 바꾸기
+var numStr = "10"
+Int(numStr)
+
+var numStr1 = "hello"
+Int(numStr1)
+
+var number5 = 6
+var str4 = "hello"
+"\(str4)! \(number5)"
+String(number5)
+//Integer
+3/2
+//double / float
+3.0/2.0
+//double to Integer
+Int(3.0/2.0)
+
+
+
+
+
+
+
+
+
+
+
+
 
